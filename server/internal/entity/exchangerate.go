81a609be05e1522030596bf8878cb706a74b90a4
antoniofmoliveira <antoniofmoliveia@outlook.com>
2024-10-15 12:58:32 -0300

    feat: add exchangerate entity validation

--- server/internal/entity/exchangerate.go
+++ server/internal/entity/exchangerate.go
@@ -1,5 +1,7 @@
 package entity
 
+import "errors"
+
 type ExchangeRate struct {
 	ID         ID     `json:"id"`
 	Code       string `json:"code"`
@@ -39,9 +41,59 @@ func NewExchangeRate(code, codein, name, high, low, varBid, pctChange, bid, ask,
 	return exchangeRate, err
 }
 
+var ErrIDInvalid = errors.New("id is invalid")
+var ErrCodeEmpty = errors.New("code is required")
+var ErrCodeinEmpty = errors.New("codein is required")
+var ErrNameEmpty = errors.New("name is required")
+var ErrHighEmpty = errors.New("high is required")
+var ErrLowEmpty = errors.New("low is required")
+var ErrVarBidEmpty = errors.New("varBid is required")
+var ErrPctChangeEmpty = errors.New("pctChange is required")
+var ErrBidEmpty = errors.New("bid is required")
+var ErrAskEmpty = errors.New("ask is required")
+var ErrTimestampEmpty = errors.New("timestamp is required")
+var ErrCreateDateEmpty = errors.New("create_date is required")
+
 // Validate checks if the fields of the ExchangeRate are valid, and returns an error
 // if any of them are invalid. It does not check if the ID field is valid, as it is
 // set by the NewExchangeRate function.
 func (er ExchangeRate) Validate() error {
+	_, err := ParseID(er.ID.String())
+	if err != nil {
+		return ErrIDInvalid
+	}
+	if er.Code == "" {
+		return ErrCodeEmpty
+	}
+	if er.Codein == "" {
+		return ErrCodeinEmpty
+	}
+	if er.Name == "" {
+		return ErrNameEmpty
+	}
+	if er.High == "" {
+		return ErrHighEmpty
+	}
+	if er.Low == "" {
+		return ErrLowEmpty
+	}
+	if er.VarBid == "" {
+		return ErrVarBidEmpty
+	}
+	if er.PctChange == "" {
+		return ErrPctChangeEmpty
+	}
+	if er.Bid == "" {
+		return ErrBidEmpty
+	}
+	if er.Ask == "" {
+		return ErrAskEmpty
+	}
+	if er.Timestamp == "" {
+		return ErrTimestampEmpty
+	}
+	if er.CreateDate == "" {
+		return ErrCreateDateEmpty
+	}
 	return nil
 }
